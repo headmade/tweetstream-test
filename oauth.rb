2b1bbfec505098c39b0f839728cf488d72e79743
Anton Taraev <anti191@gmail.com>
2013-09-05 17:20:01 +0400

    init commit

---
+++ oauth.rb
@@ -0,0 +1,24 @@
+require 'tweetstream'
+auth = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/credentials.yml"))
+
+TweetStream.configure do |config|
+  config.consumer_key       = auth['consumer_key']
+  config.consumer_secret    = auth['consumer_secret']
+  config.oauth_token        = auth['oauth_token']
+  config.oauth_token_secret = auth['oauth_token_secret']
+  config.auth_method        = :oauth
+
+end
+
+EM.run do
+  client = TweetStream::Client.new
+
+  client.on_error do |message|
+    puts message
+  end
+
+
+  client.track(%w(kzn kazan)) do |status|
+    puts status.inspect
+  end
+end
