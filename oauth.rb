916986803156199171c0edc5e023e55e66d8ba1b
lev <relevv@gmail.com>
2013-09-08 15:37:36 +0400

    Вынес track_words в отдельную переменную

--- oauth.rb
+++ oauth.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require 'tweetstream'
 require 'yaml'
 auth = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/credentials.yml"))
@@ -8,7 +10,6 @@ TweetStream.configure do |config|
   config.oauth_token        = auth['oauth_token']
   config.oauth_token_secret = auth['oauth_token_secret']
   config.auth_method        = :oauth
-
 end
 
 client = TweetStream::Client.new
@@ -18,6 +19,11 @@ client.on_error do |message|
 end
 
 
-client.track(%w(kzn kazan)) do |status|
+track_words = %w(it_ulsk ulsk dtp dengoroda мэрмосквы)
+track_words = %w(мэрмосквы выборымэра чистыевыборы говорючестно выборыМО 8сентября)
+puts "Tracking: " + track_words.join(',')
+
+client.track(track_words) do |status|
   puts status.inspect
 end
+
