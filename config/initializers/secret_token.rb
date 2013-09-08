338ba535e2934c54e064d5e949e0e0762c3c97d7
Koresh <koresh.x9@gmail.com>
2013-09-08 21:25:23 +0400

    Started progect and begined scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetstreamTest::Application.config.secret_key_base = 'e2d3dc772845715b9151e750ed7626f54e6140624f88d531f6882e3dc247a10c558b667a06509c9472160af372df5d93880b92cd1647b246794f4c0ba9b06e42'
