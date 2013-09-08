51ee3873a171befc565e6a90b64cb89397e42d85
Anton Taraev <anti191@gmail.com>
2013-09-09 01:33:42 +0400

    Add unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,9 @@ set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 require "bundler/capistrano"
 require "rvm/capistrano"
+require 'capi/unicorn'
 
 load 'config/deploy/settings.rb'
 
 before 'deploy:setup', 'rvm:install_ruby'
+after "deploy:restart", "unicorn:restart"
