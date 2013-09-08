b92eff17d1f3c41091128f550824c46b3ada719a
lev <relevv@gmail.com>
2013-09-08 21:44:53 +0400

    Добавил сохранение в базу твитов и hashtags

--- oauth.rb
+++ oauth.rb
@@ -3,34 +3,69 @@
 require 'tweetstream'
 require 'yaml'
 require 'pg'
+require "unicode_utils/downcase"
 
-auth = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/credentials.yml"))
+config_auth   = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/credentials.yml"))
+config_filter = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/filter.yml"))
+puts "FILTER: " + config_filter.inspect
 
 TweetStream.configure do |config|
-  config.consumer_key       = auth['consumer_key']
-  config.consumer_secret    = auth['consumer_secret']
-  config.oauth_token        = auth['oauth_token']
-  config.oauth_token_secret = auth['oauth_token_secret']
+  config.consumer_key       = config_auth['consumer_key']
+  config.consumer_secret    = config_auth['consumer_secret']
+  config.oauth_token        = config_auth['oauth_token']
+  config.oauth_token_secret = config_auth['oauth_token_secret']
   config.auth_method        = :oauth
 end
 
-CREATE_TABLE = <<EOS
-DROP TABLE IF EXISTS tweets_raw;
+CREATE_TABLE_TWEET = <<EOS
+DROP TABLE IF EXISTS tweets_raw CASCADE;
 CREATE TABLE tweets_raw (
-  id SERIAL PRIMARY KEY,
+  id          SERIAL PRIMARY KEY,
   created_at  TIMESTAMP       NOT NULL,
   tweet       VARCHAR         NOT NULL,
-  hashtags    VARCHAR         NOT NULL,
-  track_words TEXT            NOT NULL,
   raw         TEXT            NOT NULL
 );
 EOS
 
-INSERT_TWEET = "INSERT INTO tweets_raw (created_at,tweet,hashtags,track_words,raw)
-                                 VALUES(NOW(),$1,$2,$3,$4)"
+CREATE_TABLE_HASHTAGS = <<EOS
+DROP TABLE IF EXISTS hashtags CASCADE;
+CREATE TABLE hashtags (
+  id          SERIAL PRIMARY KEY,
+  created_at  TIMESTAMP       NOT NULL,
+  hashtag     VARCHAR         NOT NULL
+);
+CREATE UNIQUE INDEX ON hashtags(hashtag);
+EOS
+
+CREATE_TABLE_TWEET_HASHTAGS = <<EOS
+DROP TABLE IF EXISTS tweet_hashtags;
+CREATE TABLE tweet_hashtags (
+  id         SERIAL PRIMARY KEY,
+  tweet_id   INTEGER NOT NULL REFERENCES tweets_raw(id) ON DELETE CASCADE,
+  hashtag_id INTEGER NOT NULL REFERENCES hashtags(id)   ON DELETE CASCADE
+);
+CREATE INDEX ON tweet_hashtags(hashtag_id);
+CREATE INDEX ON tweet_hashtags(tweet_id);
+EOS
+
+INSERT_TWEET   = "INSERT INTO tweets_raw (created_at,tweet,raw) VALUES (NOW(),$1,$2) RETURNING id"
+INSERT_HASHTAG = "INSERT INTO hashtags   (created_at,hashtag) VALUES (NOW(),$1) RETURNING id"
+
+INSERT_TWEET_HASHTAG = "INSERT INTO tweet_hashtags  (tweet_id,hashtag_id) VALUES "
 
 conn = PG.connect( user: 'tweetstream_test', dbname: 'tweetstream_test' )
-conn.exec(CREATE_TABLE)
+conn.exec(CREATE_TABLE_TWEET)
+conn.exec(CREATE_TABLE_HASHTAGS)
+conn.exec(CREATE_TABLE_TWEET_HASHTAGS)
+
+hashtags_known = {}
+res = conn.exec('SELECT id, hashtag FROM hashtags')
+res.each_row do |id,hashtag|
+puts hashtag.inspect
+  hashtags_known[hashtag] = id
+end
+
+puts "Known hashtags: " + hashtags_known.inspect
 
 client = TweetStream::Client.new
 
@@ -39,15 +74,28 @@ client.on_error do |message|
 end
 
 
-track_words = %w(it_ulsk ulsk dtp dengoroda мэрмосквы)
-track_words = %w(мэрмосквы выборымэра чистыевыборы говорючестно выборыМО 8сентября)
-puts "Tracking: " + track_words.join(',')
+#track_words = %w(it_ulsk ulsk dtp dengoroda мэрмосквы)
+#track_words = %w(мэрмосквы выборымэра чистыевыборы говорючестно выборыМО 8сентября)
+track_words = config_filter['words']
+puts "Tracking: " + track_words
 
-client.track(track_words) do |status_obj|
-  puts status_obj.attrs.inspect
-  tweet = status_obj.text
-  hashtags = status_obj.attrs[:entities][:hashtags].map{ |ht| ht[:text] }.inspect
-  raw = status_obj.attrs.inspect
-  conn.exec_params( INSERT_TWEET, [tweet,hashtags,track_words,raw] )
+client.track(track_words) do |status|
+#  puts status.attrs.inspect
+  tweet = status.text
+  puts "#{status.user.name}: #{tweet}"
+  raw = status.attrs.inspect
+  res = conn.exec_params( INSERT_TWEET, [tweet,raw] )
+  tweet_id = res[0]['id']
+
+  hashtags_sql = status.attrs[:entities][:hashtags].map do |ht|
+    hashtag = UnicodeUtils.downcase(ht[:text])
+    hashtag_id = hashtags_known[hashtag]
+    unless hashtag_id
+      hashtag_id = conn.exec_params( INSERT_HASHTAG, [hashtag] )[0]['id']
+      hashtags_known[hashtag] = hashtag_id
+    end
+    ['(',tweet_id,',',hashtag_id,')'].join
+  end
+  conn.exec_params( INSERT_TWEET_HASHTAG + hashtags_sql.join(',') ) if hashtags_sql.any?
 end
 
