6fe67ee16882218b3f14ec356a096d913a5a928f
Anton Taraev <anti191@gmail.com>
2013-09-09 04:06:36 +0400

    Fix ip server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,9 +6,9 @@ set :use_sudo, false
 
 set :deploy_to, "/u/apps/tweetstream"
 
-role :web, "192.241.186.201"                          # Your HTTP server, Apache/etc
-role :app, "192.241.186.201"                          # This may be the same as your `Web` server
-role :db,  "192.241.186.201", :primary => true # This is where Rails migrations will run
+role :web, "192.81.223.219"                          # Your HTTP server, Apache/etc
+role :app, "192.81.223.219"                          # This may be the same as your `Web` server
+role :db,  "192.81.223.219", :primary => true # This is where Rails migrations will run
 
 set :unicorn_pid_file, "#{deploy_to}/shared/pids/unicorn.pid"
 #set :unicorn_conf, "#{deploy_to}/current/config/unicorn.conf"
