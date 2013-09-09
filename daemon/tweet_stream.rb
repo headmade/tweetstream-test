124d6e3b76689017103da0072d7d3e0e56caa03f
lev <relevv@gmail.com>
2013-09-10 00:01:25 +0400

    Превратил tweet-stream клиента в демона, дружелюбного к не-AR базам

--- daemon/tweet_stream.rb
+++ daemon/tweet_stream.rb
@@ -5,7 +5,41 @@ require 'tweetstream'
 require 'yaml'
 require 'pg'
 
-config_auth   = YAML::load(File.open("#{File.expand_path(File.dirname(__FILE__))}/../config/credentials.yml"))
+BASE_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+CONF_DIR = File.join(BASE_DIR,'config')
+
+require File.join(CONF_DIR, 'environment')
+
+INSERT_TWEET   = 'INSERT INTO tweets   (created_at,tweet,raw) VALUES (NOW(),$1,$2) RETURNING id'
+INSERT_HASHTAG = 'INSERT INTO hashtags (created_at,text)      VALUES (NOW(),$1)    RETURNING id'
+
+INSERT_TWEET_HASHTAG = 'INSERT INTO tweet_hashtags (tweet_id,hashtag_id) VALUES '
+
+
+# --- initialize config (track_phrases) and last state (hashtags_known) ---
+
+conn = PG.connect( user: 'dev', dbname: 'tweetstream_development' )
+
+track_phrases = []
+conn.exec('SELECT id,text FROM track_phrases').each_row do |id,text|
+  track_phrases << text
+end
+puts 'Track phrases: ' + track_phrases.inspect
+
+hashtags_known = {}
+conn.exec('SELECT id, text FROM hashtags').each_row do |id,hashtag|
+  hashtags_known[hashtag] = id
+end
+#puts 'Known hashtags: ' + hashtags_known.inspect
+
+# need this before going daemon
+conn.close
+conn = nil
+
+
+# --- initialize tweetstream ---
+
+config_auth = YAML::load_file(File.join(CONF_DIR,'credentials.yml'))
 
 TweetStream.configure do |config|
   config.consumer_key       = config_auth['consumer_key']
@@ -15,38 +49,19 @@ TweetStream.configure do |config|
   config.auth_method        = :oauth
 end
 
+daemon = TweetStream::Daemon.new('tweetstream', log_output: true)
 
-INSERT_TWEET   = "INSERT INTO tweets   (created_at,tweet,raw) VALUES (NOW(),$1,$2) RETURNING id"
-INSERT_HASHTAG = "INSERT INTO hashtags (created_at,text)      VALUES (NOW(),$1)    RETURNING id"
-
-INSERT_TWEET_HASHTAG = "INSERT INTO tweet_hashtags (tweet_id,hashtag_id) VALUES "
-
-conn = PG.connect( user: 'dev', dbname: 'tweetstream_development' )
-
-track_phrases = []
-conn.exec('SELECT id,text FROM track_phrases').each_row do |id,text|
-  track_phrases << text
-end
-puts "Track phrases: " + track_phrases.inspect
-
-hashtags_known = {}
-conn.exec('SELECT id, text FROM hashtags').each_row do |id,hashtag|
-  hashtags_known[hashtag] = id
-end
-puts "Known hashtags: " + hashtags_known.inspect
-
-client = TweetStream::Client.new
-
-client.on_error do |message|
+daemon.on_error do |message|
   puts message
 end
 
-
-client.track(track_phrases) do |status|
+daemon.track(track_phrases) do |status|
+  conn ||= PG.connect( user: 'dev', dbname: 'tweetstream_development' )
 #  puts status.attrs.inspect
   puts "#{status.user.name}: #{status.text}"
   tweet = status.text
   raw = status.attrs.inspect
+
   tweet_id = conn.exec_params( INSERT_TWEET, [tweet,raw] )[0]['id']
 
   hashtags_sql = status.attrs[:entities][:hashtags].map do |ht|
