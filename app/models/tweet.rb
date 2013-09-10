ddd546e8034833414b72243c8f82797a793b839f
lev <relevv@gmail.com>
2013-09-10 22:32:28 +0400

    Добавил подсчёт tweet-статистики

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
 
+  def self.count_for_last(seconds)
+     self.where( "created_at > NOW() + INTERVAL '? seconds'", Time.now.utc_offset() - seconds ).count
+  end
 end
